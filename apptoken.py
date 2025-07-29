dcbacc166e5e1c563706fb18b81fb188eea1bef6
Suresh Thalluri <suresh.thalluri@stillwater.com>
2025-07-29 00:08:57 -0600

    Adding sample token generation

---
+++ apptoken.py
@@ -0,0 +1,6 @@
+from app.core.jwt_validator import jwt_validator
+
+# Create a token
+payload = {"user_id": 123, "username": "john_doe"}
+token = jwt_validator.create_token(payload)
+print(f"+++++++ Token: {token}")
